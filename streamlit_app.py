72d8d325be9a8d84f82dc21f37c1cb9821e0d3bd
Linda Riquelme <70336743+linriquelme-tech@users.noreply.github.com>
2023-05-29 15:14:54 -0400

    Update streamlit_app.py
    test snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,12 @@ streamlit.text("The selected fruit is: {}".format(f_choice));
 f_response = requests.get("https://fruityvice.com/api/fruit/"+f_choice); # watermelon info
 
 streamlit.dataframe(pd.json_normalize(f_response.json()));
+
+
+#database (snowflake) connection
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]);
+my_cur = my_cnx.cursor();
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()");
+my_data_row = my_cur.fetchone();
+streamlit.text("Hello from Snowflake:");
+streamlit.text(my_data_row);
