adf78945a9698ba5ba74aa7fb2e1cdb0217bfd91
Linda Riquelme <70336743+linriquelme-tech@users.noreply.github.com>
2023-05-30 05:33:51 -0400

    Update streamlit_app.py
    moved functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,24 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-# FUNCTIONS
-def get_fruityvice_data(this_choice):
-  f_response = requests.get("https://fruityvice.com/api/fruit/"+this_choice);
-  return pd.json_normalize(f_response.json())
-
-def get_fruit_load_list():
-  with my_cnx.cursor() as my_cur:
-    #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()");
-    my_cur.execute("SELECT * FROM fruit_load_list");
-    #my_data_row = my_cur.fetchone();
-    return my_cur.fetchall();
-  
-def insert_row_snowflake(new_fruit):
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("INSERT INTO fruit_load_list VALUES('from streamlit')")
-    return "Thanks for adding" + new_fruit;
-    
-
 streamlit.title('My parents new healthy diner!');
 streamlit.header('Breakfast Menu');
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal');
@@ -47,8 +29,27 @@ try:
 except URLError as e:
   streamlit.error();
 
-streamlit.text("The fruit load list contains:");
 #database (snowflake) connection
+
+# FUNCTIONS
+def get_fruityvice_data(this_choice):
+  f_response = requests.get("https://fruityvice.com/api/fruit/"+this_choice);
+  return pd.json_normalize(f_response.json())
+
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()");
+    my_cur.execute("SELECT * FROM fruit_load_list");
+    #my_data_row = my_cur.fetchone();
+    return my_cur.fetchall();
+  
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("INSERT INTO fruit_load_list VALUES('from streamlit')")
+    return "Thanks for adding" + new_fruit;
+    
+
+streamlit.text("The fruit load list contains:");
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]);
   my_data_rows = get_fruit_load_list();
