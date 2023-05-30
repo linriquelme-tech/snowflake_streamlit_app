35f966bd6d3138020d6088fd96c220eaa533db9d
Linda Riquelme <70336743+linriquelme-tech@users.noreply.github.com>
2023-05-30 05:41:56 -0400

    Update streamlit_app.py
    save fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ def get_fruit_load_list():
   
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("INSERT INTO fruit_load_list VALUES('from streamlit')")
+    my_cur.execute("INSERT INTO fruit_load_list VALUES('"+new_fruit+"')")
     return "Thanks for adding" + new_fruit;
     
 
