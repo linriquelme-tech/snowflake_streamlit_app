dc70b9a03b2ac05ca68c9570b6ef673320e72544
Linda Riquelme <70336743+linriquelme-tech@users.noreply.github.com>
2023-05-30 05:42:32 -0400

    Update streamlit_app.py
    added whitespace

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ def get_fruit_load_list():
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("INSERT INTO fruit_load_list VALUES('"+new_fruit+"')")
-    return "Thanks for adding" + new_fruit;
+    return "Thanks for adding " + new_fruit;
     
 
 streamlit.text("The fruit load list contains:");
